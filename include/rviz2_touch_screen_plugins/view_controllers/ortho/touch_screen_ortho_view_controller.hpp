3ca86788a2bbc3da09e598928fca1115c8664482
Julien Kindle <julien.kindle@hilti.com>
2023-05-19 11:27:17 +0200

    Added options to toggle functionality

--- include/rviz2_touch_screen_plugins/view_controllers/ortho/touch_screen_ortho_view_controller.hpp
+++ include/rviz2_touch_screen_plugins/view_controllers/ortho/touch_screen_ortho_view_controller.hpp
@@ -39,77 +39,83 @@
 
 namespace rviz_rendering
 {
-class Shape;
+  class Shape;
 }
 
 namespace rviz_common
 {
-class ViewportMouseEvent;
-namespace properties
-{
-class FloatProperty;
-}
+  class ViewportMouseEvent;
+  namespace properties
+  {
+    class FloatProperty;
+  }
 }
 
 namespace Ogre
 {
-class SceneNode;
+  class SceneNode;
 }
 
 namespace rviz2_touch_screen_plugins
 {
-namespace view_controllers
-{
+  namespace view_controllers
+  {
 
-class  TouchScreenTopDownOrtho : public
-  rviz_common::FramePositionTrackingViewController
-{
-  Q_OBJECT
+    class TouchScreenTopDownOrtho : public rviz_common::FramePositionTrackingViewController
+    {
+      Q_OBJECT
 
-public:
-  TouchScreenTopDownOrtho();
-  ~TouchScreenTopDownOrtho() override = default;
+    public:
+      TouchScreenTopDownOrtho();
+      ~TouchScreenTopDownOrtho() override = default;
 
-  void onInitialize() override;
+      void onInitialize() override;
 
-  void handleMouseEvent(rviz_common::ViewportMouseEvent & evt) override;
+      void handleMouseEvent(rviz_common::ViewportMouseEvent &evt) override;
 
-  void lookAt(const Ogre::Vector3 & point_rel_world) override;
+      void lookAt(const Ogre::Vector3 &point_rel_world) override;
 
-  void reset() override;
+      void reset() override;
 
-  /** @brief Configure the settings of this view controller to give,
-   * as much as possible, a similar view as that given by the
-   * @a source_view.
-   *
-   * @a source_view must return a valid @c Ogre::Camera* from getCamera(). */
-  void mimic(ViewController * source_view) override;
+      /** @brief Configure the settings of this view controller to give,
+       * as much as possible, a similar view as that given by the
+       * @a source_view.
+       *
+       * @a source_view must return a valid @c Ogre::Camera* from getCamera(). */
+      void mimic(ViewController *source_view) override;
 
-  void move(float x, float y);
+      void move(float x, float y);
 
-  void update(float dt, float ros_dt) override;
+      void update(float dt, float ros_dt) override;
 
-protected:
-  void onTargetFrameChanged(
-    const Ogre::Vector3 & old_reference_position,
-    const Ogre::Quaternion & old_reference_orientation) override;
+    protected:
+      void onTargetFrameChanged(
+          const Ogre::Vector3 &old_reference_position,
+          const Ogre::Quaternion &old_reference_orientation) override;
 
-  /** Set the camera orientation based on angle_. */
-  void orientCamera();
+      /** Set the camera orientation based on angle_. */
+      void orientCamera();
 
-  void setPosition(const Ogre::Vector3 & pos_rel_target);
-  void updateCamera();
-  Ogre::SceneNode * getCameraParent(Ogre::Camera * camera);
-  void renderOnMove();
+      void setPosition(const Ogre::Vector3 &pos_rel_target);
+      void updateCamera();
+      Ogre::SceneNode *getCameraParent(Ogre::Camera *camera);
+      void renderOnMove();
 
-  rviz_common::properties::FloatProperty * scale_property_;
-  rviz_common::properties::FloatProperty * angle_property_;
-  rviz_common::properties::FloatProperty * x_property_;
-  rviz_common::properties::FloatProperty * y_property_;
-  bool dragging_;
-};
+      rviz_common::properties::FloatProperty *scale_property_;
+      rviz_common::properties::FloatProperty *angle_property_;
+      rviz_common::properties::FloatProperty *x_property_;
+      rviz_common::properties::FloatProperty *y_property_;
+      rviz_common::properties::BoolProperty *track_ori_property_;
+      rviz_common::properties::FloatProperty *controls_width_property_;
+      rviz_common::properties::BoolProperty *en_move_property_;
+      rviz_common::properties::BoolProperty *en_scale_property_;
+      rviz_common::properties::BoolProperty *en_rot_property_;
+      rviz_common::properties::BoolProperty *en_reset_property_;
 
-}  // namespace view_controllers
-}  // namespace rviz2_touch_screen_plugins
+      bool dragging_;
+    };
 
-#endif  // rviz2_touch_screen_plugins__VIEW_CONTROLLERS__ORTHO__FIXED_ORIENTATION_ORTHO_VIEW_CONTROLLER_HPP_  // NOLINT
+  } // namespace view_controllers
+} // namespace rviz2_touch_screen_plugins
+
+#endif // rviz2_touch_screen_plugins__VIEW_CONTROLLERS__ORTHO__FIXED_ORIENTATION_ORTHO_VIEW_CONTROLLER_HPP_  // NOLINT
--- src/view_controllers/ortho/touch_screen_ortho_view_controller.cpp
+++ src/view_controllers/ortho/touch_screen_ortho_view_controller.cpp
@@ -45,192 +45,233 @@
 
 namespace rviz2_touch_screen_plugins
 {
-namespace view_controllers
-{
+  namespace view_controllers
+  {
 
-static const float ORTHO_VIEW_CONTROLLER_CAMERA_Z = 500;
+    static const float ORTHO_VIEW_CONTROLLER_CAMERA_Z = 500;
 
-TouchScreenTopDownOrtho::TouchScreenTopDownOrtho()
-: dragging_(false)
-{
-  scale_property_ = new rviz_common::properties::FloatProperty(
-    "Scale", 10, "How much to scale up the size of things in the scene.", this);
-  angle_property_ = new rviz_common::properties::FloatProperty(
-    "Angle", 0, "Angle around the Z axis to rotate.", this);
-  x_property_ = new rviz_common::properties::FloatProperty(
-    "X", 0, "X component of camera position.", this);
-  y_property_ = new rviz_common::properties::FloatProperty(
-    "Y", 0, "Y component of camera position.", this);
-}
+    TouchScreenTopDownOrtho::TouchScreenTopDownOrtho()
+        : dragging_(false)
+    {
+      scale_property_ = new rviz_common::properties::FloatProperty(
+          "Scale", 10, "How much to scale up the size of things in the scene.", this);
+      angle_property_ = new rviz_common::properties::FloatProperty(
+          "Angle", 0, "Angle around the Z axis to rotate.", this);
+      x_property_ = new rviz_common::properties::FloatProperty(
+          "X", 0, "X component of camera position.", this);
+      y_property_ = new rviz_common::properties::FloatProperty(
+          "Y", 0, "Y component of camera position.", this);
+      track_ori_property_ = new rviz_common::properties::BoolProperty(
+          "Track Orientation", false, "Set if orientation of target frame is tracked.", this);
+      controls_width_property_ = new rviz_common::properties::FloatProperty(
+          "Control field width", 0.05, "Sets the width of the control fields (relative to width / height of 3D viewer).", this);
+      controls_width_property_->setMin(0.0);
+      controls_width_property_->setMax(1.0);
+      en_move_property_ = new rviz_common::properties::BoolProperty(
+          "Enable movement", true, "Set if movement is enabled.", this);
+      en_scale_property_ = new rviz_common::properties::BoolProperty(
+          "Enable scaling", true, "Set if scaling is enabled.", this);
+      en_rot_property_ = new rviz_common::properties::BoolProperty(
+          "Enable rotation", true, "Set if rotation is enabled.", this);
+      en_reset_property_ = new rviz_common::properties::BoolProperty(
+          "Enable reset", true, "Set if reset is enabled.", this);
+    }
 
-void TouchScreenTopDownOrtho::onInitialize()
-{
-  FramePositionTrackingViewController::onInitialize();
+    void TouchScreenTopDownOrtho::onInitialize()
+    {
+      FramePositionTrackingViewController::onInitialize();
 
-  camera_->setProjectionType(Ogre::PT_ORTHOGRAPHIC);
-  auto camera_parent = getCameraParent(camera_);
-  camera_parent->setFixedYawAxis(false);
-  invert_z_->hide();
-}
+      camera_->setProjectionType(Ogre::PT_ORTHOGRAPHIC);
+      auto camera_parent = getCameraParent(camera_);
+      camera_parent->setFixedYawAxis(false);
+      invert_z_->hide();
+    }
 
-void TouchScreenTopDownOrtho::reset()
-{
-  scale_property_->setFloat(10);
-  angle_property_->setFloat(-M_PI_2);
-  x_property_->setFloat(0);
-  y_property_->setFloat(0);
-}
+    void TouchScreenTopDownOrtho::reset()
+    {
+      scale_property_->setFloat(10);
+      angle_property_->setFloat(-M_PI_2);
+      x_property_->setFloat(0);
+      y_property_->setFloat(0);
+    }
 
-void TouchScreenTopDownOrtho::handleMouseEvent(rviz_common::ViewportMouseEvent & event)
-{
-  if (event.shift()) {
-    setStatus("<b>Left-Click:</b> Move X/Y.");
-  } else {
-    setStatus(
-      "<b>Left-Click:</b> Rotate.  <b>Middle-Click:</b> Move X/Y. "
-      " <b>Right-Click:</b> Zoom.  <b>Shift:</b> More options.");
-  }
+    void TouchScreenTopDownOrtho::handleMouseEvent(rviz_common::ViewportMouseEvent &event)
+    {
+      // Get the actual dimensions of the viewport
+      int viewport_width = camera_->getViewport()->getActualWidth();
+      int viewport_height = camera_->getViewport()->getActualHeight();
 
-   // Get the actual dimensions of the viewport
-  int viewport_width = camera_->getViewport()->getActualWidth();
-  int viewport_height = camera_->getViewport()->getActualHeight();
+      int32_t diff_x = 0;
+      int32_t diff_y = 0;
 
-  int32_t diff_x = 0;
-  int32_t diff_y = 0;
+      if (event.type == QEvent::MouseButtonPress)
+      {
+        dragging_ = true;
+      }
+      else if (event.type == QEvent::MouseButtonRelease)
+      {
+        dragging_ = false;
+      }
+      else if (dragging_ && event.type == QEvent::MouseMove)
+      {
+        diff_x = event.x - event.last_x;
+        diff_y = event.y - event.last_y;
+        renderOnMove();
+      }
 
+      // Get configured control fields width
+      double cfw = controls_width_property_->getFloat();
 
-  if (event.type == QEvent::MouseButtonPress) {
-    dragging_ = true;
-  } else if (event.type == QEvent::MouseButtonRelease) {
-    dragging_ = false;
-  } else if (dragging_ && event.type == QEvent::MouseMove) {
-    diff_x = event.x - event.last_x;
-    diff_y = event.y - event.last_y;
-    renderOnMove();
-  }
+      // Top right corner click causes reset
+      if (en_reset_property_->getBool() && event.type == QEvent::MouseButtonPress && event.left() && event.x >= (1.0 - cfw) * viewport_width && event.y <= cfw * viewport_height)
+      {
+        x_property_->setFloat(0);
+        y_property_->setFloat(0);
+      }
+      // Right side of screen used to scroll
+      else if (en_scale_property_->getBool() && event.left() && event.x >= (1.0 - cfw) * viewport_width)
+      {
+        setCursor(Zoom);
+        scale_property_->multiply(1.0f - diff_y * 0.01f);
+      }
+      // Top side of screen used to rotate
+      else if (en_rot_property_->getBool() && event.left() && event.y <= cfw * viewport_height)
+      {
+        setCursor(Rotate2D);
+        angle_property_->add(diff_x * 0.005f);
+        orientCamera();
+      }
 
-  if (event.type == QEvent::MouseButtonPress && event.left() && event.x > 0.95*viewport_width && event.y < 0.05*viewport_height) {
-angle_property_->setFloat(-M_PI_2);
-  x_property_->setFloat(0);
-  y_property_->setFloat(0);
+      // Rest of screen used to move
+      else if (en_move_property_->getBool())
+      {
+        setCursor(MoveXY);
+        float scale = scale_property_->getFloat();
+        move(-diff_x / scale, diff_y / scale);
+      }
+      else
+      {
+        setCursor(Default);
+      }
+    }
 
-  } else if (event.left() && event.x > 0.95*viewport_width) {
-setCursor(Zoom);
-    scale_property_->multiply(1.0f - diff_y * 0.01f);
+    void TouchScreenTopDownOrtho::renderOnMove()
+    {
+      context_->queueRender();
+      emitConfigChanged();
+    }
 
-  }else{
-setCursor(MoveXY);
-    float scale = scale_property_->getFloat();
-    move(-diff_x / scale, diff_y / scale);
-  }
+    void TouchScreenTopDownOrtho::orientCamera()
+    {
+      auto camera_parent = getCameraParent(camera_);
+      camera_parent->setOrientation(
+          Ogre::Quaternion(Ogre::Radian(angle_property_->getFloat()), Ogre::Vector3::UNIT_Z));
+    }
 
-}
+    void TouchScreenTopDownOrtho::mimic(ViewController *source_view)
+    {
+      FramePositionTrackingViewController::mimic(source_view);
 
-void TouchScreenTopDownOrtho::renderOnMove()
-{
-  context_->queueRender();
-  emitConfigChanged();
-}
+      if (source_view->getClassId() == "rviz2_touch_screen_plugins/TouchScreenTopDownOrtho")
+      {
+        auto source_ortho = qobject_cast<TouchScreenTopDownOrtho *>(source_view);
+        scale_property_->setFloat(source_ortho->scale_property_->getFloat());
+        angle_property_->setFloat(source_ortho->angle_property_->getFloat());
+        x_property_->setFloat(source_ortho->x_property_->getFloat());
+        y_property_->setFloat(source_ortho->y_property_->getFloat());
+      }
+      else if (source_view->getFocalPointStatus().exists_)
+      {
+        setPosition(source_view->getFocalPointStatus().value_);
+      }
+      else
+      {
+        // if the previous view does not have a focal point and is not the same as this, the camera is
+        // placed at (x, y, ORTHO_VIEW_CONTROLLER_CAMERA_Z), where x and y are first two coordinates of
+        // the old camera position.
+        auto source_camera_parent = getCameraParent(source_view->getCamera());
+        setPosition(source_camera_parent->getPosition());
+      }
+    }
 
-void TouchScreenTopDownOrtho::orientCamera()
-{
-  auto camera_parent = getCameraParent(camera_);
-  camera_parent->setOrientation(
-    Ogre::Quaternion(Ogre::Radian(angle_property_->getFloat()), Ogre::Vector3::UNIT_Z));
-}
+    void TouchScreenTopDownOrtho::update(float dt, float ros_dt)
+    {
+      FramePositionTrackingViewController::update(dt, ros_dt);
 
-void TouchScreenTopDownOrtho::mimic(ViewController * source_view)
-{
-  FramePositionTrackingViewController::mimic(source_view);
+      if (track_ori_property_->getBool())
+      {
+        target_scene_node_->setOrientation(reference_orientation_);
+      }
 
-  if (source_view->getClassId() == "rviz2_touch_screen_plugins/TopDownOrtho") {
-    auto source_ortho = qobject_cast<TouchScreenTopDownOrtho *>(source_view);
-    scale_property_->setFloat(source_ortho->scale_property_->getFloat());
-    angle_property_->setFloat(source_ortho->angle_property_->getFloat());
-    x_property_->setFloat(source_ortho->x_property_->getFloat());
-    y_property_->setFloat(source_ortho->y_property_->getFloat());
-  } else if (source_view->getFocalPointStatus().exists_) {
-    setPosition(source_view->getFocalPointStatus().value_);
-  } else {
-    // if the previous view does not have a focal point and is not the same as this, the camera is
-    // placed at (x, y, ORTHO_VIEW_CONTROLLER_CAMERA_Z), where x and y are first two coordinates of
-    // the old camera position.
-    auto source_camera_parent = getCameraParent(source_view->getCamera());
-    setPosition(source_camera_parent->getPosition());
-  }
-}
+      updateCamera();
+    }
 
-void TouchScreenTopDownOrtho::update(float dt, float ros_dt)
-{
-  FramePositionTrackingViewController::update(dt, ros_dt);
-  updateCamera();
-}
+    void TouchScreenTopDownOrtho::lookAt(const Ogre::Vector3 &point)
+    {
+      setPosition(point - target_scene_node_->getPosition());
+    }
 
-void TouchScreenTopDownOrtho::lookAt(const Ogre::Vector3 & point)
-{
-  setPosition(point - target_scene_node_->getPosition());
-}
+    void TouchScreenTopDownOrtho::onTargetFrameChanged(
+        const Ogre::Vector3 &old_reference_position, const Ogre::Quaternion &old_reference_orientation)
+    {
+      (void)old_reference_orientation;
 
-void TouchScreenTopDownOrtho::onTargetFrameChanged(
-  const Ogre::Vector3 & old_reference_position, const Ogre::Quaternion & old_reference_orientation)
-{
-  (void) old_reference_orientation;
+      move(
+          old_reference_position.x - reference_position_.x,
+          old_reference_position.y - reference_position_.y);
+    }
 
-  move(
-    old_reference_position.x - reference_position_.x,
-    old_reference_position.y - reference_position_.y);
-}
+    void TouchScreenTopDownOrtho::updateCamera()
+    {
+      orientCamera();
 
-void TouchScreenTopDownOrtho::updateCamera()
-{
-  orientCamera();
+      float width = camera_->getViewport()->getActualWidth();
+      float height = camera_->getViewport()->getActualHeight();
 
-  float width = camera_->getViewport()->getActualWidth();
-  float height = camera_->getViewport()->getActualHeight();
+      float scale = scale_property_->getFloat();
+      float ortho_width = width / scale / 2;
+      float ortho_height = height / scale / 2;
+      Ogre::Matrix4 projection = rviz_rendering::buildScaledOrthoMatrix(
+          -ortho_width, ortho_width, -ortho_height, ortho_height,
+          camera_->getNearClipDistance(), camera_->getFarClipDistance());
+      camera_->setCustomProjectionMatrix(true, projection);
 
-  float scale = scale_property_->getFloat();
-  float ortho_width = width / scale / 2;
-  float ortho_height = height / scale / 2;
-  Ogre::Matrix4 projection = rviz_rendering::buildScaledOrthoMatrix(
-    -ortho_width, ortho_width, -ortho_height, ortho_height,
-    camera_->getNearClipDistance(), camera_->getFarClipDistance());
-  camera_->setCustomProjectionMatrix(true, projection);
+      // For Z, we use a value that seems to work very well in the past. It once was connected to
+      // half the far_clip_distance.
+      auto camera_parent = getCameraParent(camera_);
+      camera_parent->setPosition(
+          Ogre::Vector3(
+              x_property_->getFloat(), y_property_->getFloat(), ORTHO_VIEW_CONTROLLER_CAMERA_Z));
+    }
 
-  // For Z, we use a value that seems to work very well in the past. It once was connected to
-  // half the far_clip_distance.
-  auto camera_parent = getCameraParent(camera_);
-  camera_parent->setPosition(
-    Ogre::Vector3(
-      x_property_->getFloat(), y_property_->getFloat(), ORTHO_VIEW_CONTROLLER_CAMERA_Z));
-}
+    Ogre::SceneNode *TouchScreenTopDownOrtho::getCameraParent(Ogre::Camera *camera)
+    {
+      auto camera_parent = camera->getParentSceneNode();
 
-Ogre::SceneNode * TouchScreenTopDownOrtho::getCameraParent(Ogre::Camera * camera)
-{
-  auto camera_parent = camera->getParentSceneNode();
+      if (!camera_parent)
+      {
+        throw std::runtime_error("camera's parent scene node pointer unexpectedly nullptr");
+      }
+      return camera_parent;
+    }
 
-  if (!camera_parent) {
-    throw std::runtime_error("camera's parent scene node pointer unexpectedly nullptr");
-  }
-  return camera_parent;
-}
+    void TouchScreenTopDownOrtho::setPosition(const Ogre::Vector3 &pos_rel_target)
+    {
+      x_property_->setFloat(pos_rel_target.x);
+      y_property_->setFloat(pos_rel_target.y);
+    }
 
-void TouchScreenTopDownOrtho::setPosition(const Ogre::Vector3 & pos_rel_target)
-{
-  x_property_->setFloat(pos_rel_target.x);
-  y_property_->setFloat(pos_rel_target.y);
-}
+    void TouchScreenTopDownOrtho::move(float dx, float dy)
+    {
+      float angle = angle_property_->getFloat();
+      x_property_->add(dx * cos(angle) - dy * sin(angle));
+      y_property_->add(dx * sin(angle) + dy * cos(angle));
+    }
 
-void TouchScreenTopDownOrtho::move(float dx, float dy)
-{
-  float angle = angle_property_->getFloat();
-  x_property_->add(dx * cos(angle) - dy * sin(angle));
-  y_property_->add(dx * sin(angle) + dy * cos(angle));
-}
+  } // namespace view_controllers
+} // namespace rviz2_touch_screen_plugins
 
-}  // namespace view_controllers
-}  // namespace rviz2_touch_screen_plugins
-
-#include <pluginlib/class_list_macros.hpp>  // NOLINT(build/include_order)
+#include <pluginlib/class_list_macros.hpp> // NOLINT(build/include_order)
 PLUGINLIB_EXPORT_CLASS(
-  rviz2_touch_screen_plugins::view_controllers::TouchScreenTopDownOrtho,
-  rviz_common::ViewController)
+    rviz2_touch_screen_plugins::view_controllers::TouchScreenTopDownOrtho,
+    rviz_common::ViewController)
